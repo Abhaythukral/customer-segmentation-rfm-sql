9982f86289938213b14b673618c5fd475cd5a3c9
Abhaythukral <abhaythukral2003@gmail.com>
2026-08-05 18:21:45 +0530

    Update 01_create_tables.sql

--- sql/01_create_tables.sql
+++ sql/01_create_tables.sql
@@ -1,1 +1,19 @@
+--Customers Table 
+CREATE TABLE customers(
+customer_id	INT PRIMARY KEY,
+customer_name VARCHAR(20), 
+city VARCHAR(20)
+);
+
+
+--Orders Table 
+CREATE TABLE orders(
+order_id INT PRIMARY KEY,
+customer_id	INT NOT NULL,
+order_date	DATE,
+amount INT,
+CONSTRAINT fk_orders 
+ FOREIGN KEY	(customer_id)
+ REFERENCES customers(customer_id)
+);
 
