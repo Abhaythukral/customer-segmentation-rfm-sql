4842456a5bcd697843493c003513c671d29d462e
Abhaythukral <abhaythukral2003@gmail.com>
2026-08-05 17:54:44 +0530

    04_business_insights.sql

--- sql/04_business_insights.sql
+++ sql/04_business_insights.sql
@@ -1,1 +1,22 @@
+---Top 5 highest spending customers
+SELECT
+customer_name,
+SUM(amount) AS total_spent
+FROM customers c
+JOIN orders o
+ON c.customer_id=o.customer_id
+GROUP BY customer_name
+ORDER BY total_spent DESC
+LIMIT 5;
 
+
+--Total Revenue
+SELECT
+SUM(amount) AS total_revenue
+FROM orders;
+
+--Average Order Value
+SELECT
+ROUND(AVG(amount),2)
+AS average_order_value
+FROM orders;
